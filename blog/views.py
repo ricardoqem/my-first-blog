ac310a753d4d2b59750730b31f5e28fb93985a82
ricardo <ricardocg2004@gmail.com>
2020-05-28 17:09:08 -0300

    Inclui timezone

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-	posts = Post.objects.filter(title__contains='Título').order_by('create_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('create_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
